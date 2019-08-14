a48038bf47a89476e91e028a461013b893d4bd37
pspslimboy <pspslimboy98@gmail.com>
2019-08-13 22:29:46 -0400

    gnu: Set 1/256 microstepping

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -718,7 +718,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 96 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 1280, 1280, 6400, 1536 }
 
 /**
  * Default Max Feed Rate (mm/s)
